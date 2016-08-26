529d4ef3b1671fd41930db80e8332bdeb18eb787
Alvin Dickson <dickson.alvin@gmail.com>
2016-08-25 22:30:24 -0400

    Showing movie info updating grid

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 var Movie = React.createClass({
-  
   render: function() {
-    console.log(this.props);
     return (
       <div className="container">
-        <h2>{this.props.title}</h2>
-        <img src={`http://image.tmdb.org/t/p/w185/${this.props.poster_path}`} alt={this.props.name}/>
-        <div className="movie-description">
+        <MovieInfo {...this.props.movie} />
+        <Reviews reviews={this.props.reviews} />
+      </div>
+    );
+  }
+});
+
+var MovieInfo = React.createClass({
+  render: function() {
+    return (
+      <div className="grid">
+        <div className="col-3-10">
+          <h2>{this.props.title}</h2>
+          <img src={`http://image.tmdb.org/t/p/w185/${this.props.poster_path}`} alt={this.props.name}/>
+        </div>
+        <div className="movie-description col-3-10">
           <h2>About the movie</h2>
           <p>
             {this.props.overview}
@@ -17,8 +28,37 @@ var Movie = React.createClass({
       </div>
     );
   }
-
 });
 
+var Reviews = React.createClass({
+  render: function() {
+    var reviews = this.props.reviews.map(function(review){
+      return (
+        <Review key={review.id} {...review} />
+      );
+    });
+    return (
+      <div className="grid">
+        <div className="customer-reviews col-offset-3-10">
+          <h2>Customer Reviews</h2>
+          {reviews}
+        </div>
+      </div>
+    );
+  }
+});
+
+var Review = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div className="customer-review">
+          <h5>{this.props.author}</h5>
+          <p>{this.props.content}</p>
+        </div>
+      </div>
+    );
+  }
+});
 
 export default Movie;
--- src/containers/MovieContainer.js
+++ src/containers/MovieContainer.js
@@ -5,23 +5,40 @@ function constructFindMovie(id) {
   return `//api.themoviedb.org/3/movie/${id}?api_key=925a4602f6b05af1f8e2391a9a8e7c51`;
 }
 
+function constructMovieReviews(id) {
+  return `//api.themoviedb.org/3/movie/${id}/reviews?api_key=925a4602f6b05af1f8e2391a9a8e7c51`;
+}
+
 var MovieContainer = React.createClass({
   componentDidMount: function() {
-    fetch(constructFindMovie(this.props.params.id))
-    .then(response => response.json())
-    .then(function(movie){
-      this.setState({movie: movie})
-    }.bind(this))
+    var id = this.props.params.id;
+    Promise.all([
+        fetch(constructFindMovie(id)),
+        fetch(constructMovieReviews(id))
+      ])
+      .then(function(response){
+          var p1 = Promise.resolve(response[0].json());
+          var p2 = Promise.resolve(response[1].json());
+          return Promise.all([p1, p2]);
+      }).then(function(data){
+        this.setState({
+          movie: data[0],
+          reviews: data[1].results
+        });
+      }.bind(this));
   },
 
   getInitialState: function() {
     return {
-      movie: {}
+      movie: {},
+      reviews: [
+      ]
     }
   },
   render: function() {
     return (
-      <Movie {...this.state.movie}/>
+      <Movie movie={this.state.movie}
+            reviews={this.state.reviews} />
     )
   }
 });
