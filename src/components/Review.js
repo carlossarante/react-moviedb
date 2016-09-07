fde94b42b738ac14865f4a35e96c1e2174a74216
Alvin Dickson <dickson.alvin@gmail.com>
2016-09-07 20:59:15 +0000

    creating components for truncate text show more/show less

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import TruncateText from './TruncateText'
 
 var Review = React.createClass({
+  propTypes: {
+    author: React.PropTypes.string,
+    content: React.PropTypes.string
+  },
+  
   render: function() {
     return (
       <div>
-        <div className="customer-review">
+        <div className="customer-review" role="article">
           <h5>{this.props.author}</h5>
-          <p>{this.props.content}</p>
+          <TruncateText text={this.props.content} />
         </div>
       </div>
     );
---
+++ src/components/TruncateText.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+var style = {
+    color: '#61dafb',
+    cursor: 'pointer'
+}
+
+
+var TruncateText = React.createClass({
+    propTypes: {
+      text: React.PropTypes.string,
+      maxLength: React.PropTypes.number
+    },
+    
+    getDefaultProps: function() {
+      return {
+        maxLength: 255,
+        showLess: 'Show less...',
+        showMore: 'Show more...'
+      }
+    },
+    
+    getInitialState: function() {
+      return {
+        text: this.props.text,
+        cropped: true
+      }
+    },
+    
+    handleToggleText: function() {
+      this.setState({cropped: !this.state.cropped})
+    },
+    
+    renderText: function() {
+      var maxLength = this.props.maxLength;
+      var content, toggleText;
+      if(this.state.text.length > maxLength && this.state.cropped) {
+        content = this.state.text.substring(0, maxLength);
+        toggleText = this.props.showMore;
+      } else {
+        content = this.state.text;
+        toggleText = this.props.showLess;
+      }
+      return(
+        <p>
+          {content}
+          <span style={style} onClick={this.handleToggleText}> {toggleText}</span>
+        </p>  
+        );
+    },
+    
+    render: function() {
+        return (<div>{this.renderText()}</div>);  
+    }
+  
+});
+
+export default TruncateText;
